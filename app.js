41e4d192ec25b98fa08ae807e28a7b4091c6a9ee
MELTEM <meltemfsanaldi@gmail.com>
2022-09-29 00:15:47 +0300

    added color

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
 const span = document.querySelector("#span");
 
 const par = document.querySelector("p");
+
 const dice = document.querySelector(".dice");
+const body = document.querySelector("body");
+let color;
 
+dice.addEventListener("click", () => {
+  myAdvice();
+  randomColor();
+});
 
-dice.addEventListener("click", ()=>{
-    myAdvice();
- 
-})
+const myAdvice = async () => {
+  const url = "https://api.adviceslip.com/advice";
 
+  try {
+    const response = await fetch(url);
 
-
-const  myAdvice = async ()=>{
-    const url ="https://api.adviceslip.com/advice";
-    try {
-        const response =await fetch(url);
-if(!response.ok){
-    hata()
-    throw new Error("ikaz")
-}
-const data = await response.json();
-       span.innerHTML = `ADVICE #${data.slip.id}`;
-       par.innerHTML = `${data.slip.advice} `
-    } catch (error) {console.log(error)
+    if (!response.ok) {
+      hata();
+      throw new Error("ikaz");
     }
-}
 
+    const data = await response.json();
+    span.innerHTML = `ADVICE # ${data.slip.id}`;
+    par.innerHTML = `${data.slip.advice}`;
+    body.style.backgroundColor = `${color}`;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+myAdvice();
 
+const hata = () => {
+  document.querySelector(".container").innerHTML = `hatali giris`;
+};
 
-
-// myAdvice();
-const hata = ()=>{
-    document.querySelector(".container").innerHTML = `hatali giris`
-}
+const randomColor = () => {
+  let letters = "0123456789ABCDEF".split("");
+  color = `#`;
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
